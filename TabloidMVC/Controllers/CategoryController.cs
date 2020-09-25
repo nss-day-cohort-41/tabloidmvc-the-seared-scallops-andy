65e1656187f3605f62d56208298920c61b808ecd
Erik Lindstrom <elindstrom42@gmail.com>
2020-09-25 13:40:00 -0500

    stashing to test PR for tags

--- TabloidMVC/Controllers/CategoryController.cs
+++ TabloidMVC/Controllers/CategoryController.cs
@@ -63,50 +63,6 @@ namespace TabloidMVC.Controllers
             }
         }
         
-
-        //Start checking here
-        public IActionResult UserPostDetails(int id)
-        {
-            int userId = GetCurrentUserProfileId();
-            var post = _postRepository.GetUserPostById(id, userId);
-
-            if (post == null)
-            {
-                return NotFound();
-            }
-
-            return View(post);
-        }
-
-
-
-        //public IActionResult Create()
-        //{
-        //    var vm = new PostCreateViewModel();
-        //    vm.CategoryOptions = _categoryRepository.GetAll();
-        //    return View(vm);
-        //}
-
-        //[HttpPost]
-        //public IActionResult Create(PostCreateViewModel vm)
-        //{
-        //    try
-        //    {
-        //        vm.Post.CreateDateTime = DateAndTime.Now;
-        //        vm.Post.IsApproved = true;
-        //        vm.Post.UserProfileId = GetCurrentUserProfileId();
-
-        //        _postRepository.Add(vm.Post);
-
-        //        return RedirectToAction("Details", new { id = vm.Post.Id });
-        //    }
-        //    catch
-        //    {
-        //        vm.CategoryOptions = _categoryRepository.GetAll();
-        //        return View(vm);
-        //    }
-        //}
-
         public IActionResult Delete(int id)
         {
             Category category = _categoryRepository.GetCategoryById(id);
@@ -160,20 +116,6 @@ namespace TabloidMVC.Controllers
                 return View(category);
             }
         }
-        public IActionResult UserPosts()
-        {
-            int user = GetCurrentUserProfileId();
-            List<Post> userPosts = _postRepository.GetAllUserPost(user);
-
-            return View(userPosts);
-        }
-
-        private int GetCurrentUserProfileId()
-        {
-            string id = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            return int.Parse(id);
-        }
-
 
     }
 }
